104fe86b8a554e181d1f5e039b1a388a06047fcb
ioanSL <io@shardlabs.io>
2024-02-07 13:04:15 +0200

    feat: better landing page with links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,260 @@
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import Discord from '@site/static/img/discord.svg'
+import GitHub from '@site/static/img/github.svg'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
+import styled from '@emotion/styled';
+import ThemedImage from '@theme/ThemedImage';
 
-function HomepageHeader() {
+import { ArrowUpRight as LinkIcon, BookOpen, HelpCircle, Info, MessageCircle } from 'react-feather';
+
+
+export const actions = [
+  {
+    title: 'Get Started',
+    icon: Info,
+    to: '/docs/intro',
+    text: 'Learn how to start using Nimbora',
+  },
+  {
+    title: 'Integrate with Nimbora',
+    icon: BookOpen,
+    to: '/docs/sdk/overview',
+    text: 'Learn how to integrate Nimbora with your application',
+  },
+  {
+    title: 'Nimbora Smart Contracts',
+    icon: HelpCircle,
+    to: '/docs/contracts/overview',
+    text: 'Learn about Nimbora smart contracts',
+  }
+]
+
+const IconWrapper = styled.div`
+  width: 32px;
+  height: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  margin-right: 0.5rem;
+`
+
+const LinkIconWrapper = styled.div`
+  opacity: 0.25;
+`
+
+const TopSection = styled.div`
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 1rem;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`
+
+const Row = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 16px;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 1rem 0;
+  max-width: auto;
+
+  @media (max-width: 960px) {
+    grid-template-columns: 1fr;
+    padding: 1rem;
+    max-width: 100%;
+    margin: 0 1rem;
+  }
+  @media (max-width: 640px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const Card = styled.div`
+  display: flex;
+  max-height: 250px;
+  min-width: 350px;
+  padding: 1rem;
+  flex-direction: column;
+  justify-content: center;
+  cursor: pointer;
+  border: 1px solid transparent;
+  border-radius: 20px;
+  border: 1px solid var(--ifm-color-emphasis-200);
+  /* flex: 1 1 0px; */
+
+  &:hover {
+    border: 1px solid var(--ifm-color-emphasis-400);
+    box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.05);
+  }
+
+  @media (max-width: 960px) {
+    width: 100%;
+  }
+`
+
+const ShadowCard = styled(Card)`
+  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.05);
+  background-color: #ffffff10;
+  backdrop-filter: blur(10px);
+  min-height: 200px;
+  /* background-color: var(--ifm-color-emphasis-0); */
+`
+
+const DocsHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  width: 100%;
+  position: relative;
+`
+
+const StyledImage = styled(ThemedImage)`
+  position: relative;
+  z-index: -1;
+  width: 100%;
+  object-fit: cover;
+`
+
+const StyledTitleImage = styled(StyledImage)`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+  position: absolute;
+  opacity: 0.2;
+  mask-image: linear-gradient(rgba(0, 0, 0, 1), transparent);
+`
+
+const HideMedium = styled.div`
+  @media (max-width: 960px) {
+    display: none;
+  }
+`
+
+const CenterCard = styled(Card)`
+  min-width: 250px;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+
+  display: grid;
+  grid-template-columns: 48px 1fr;
+  gap: 24px;
+
+  h3 {
+    margin-bottom: 0.25rem;
+  }
+
+  p {
+    margin-bottom: 0px;
+  }
+`
+
+const StyledIcon = styled.div`
+  svg {
+    fill: var(--ifm-font-color-base);
+  }
+`
+
+export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Yield Dex Documentation
+    <Layout title={'Nimbora Documentation'} description='Technical documentation for Nimbora '>
+      <Container>
+      <DocsHeader>
+          <div
+            style={{
+              padding: '4rem 0  ',
+              textAlign: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <h1 style={{ fontWeight: 600 }}> Welcome to Nimbora Docs</h1>
+            <HideMedium>
+            </HideMedium>
+          </div>
+          {/* <StyledTitleImage
+            sources={{
+              light: useBaseUrl('/static/img/docusaurus-social-card.png'),
+              dark: useBaseUrl('/static/img/docusaurus-social-card.png'),
+            }}
+          /> */}
+          <Row>
+            {actions.map((action) => (
+              
+                <Link style={{ textDecoration: 'none' }} to={action.to}>
+                  <ShadowCard key={action.title}>
+                    <TopSection>
+                      <IconWrapper>
+                        <action.icon style={{ width: '24px' }} />
+                      </IconWrapper>
+                      <LinkIconWrapper>
+                        <LinkIcon />
+                      </LinkIconWrapper>
+                    </TopSection>
+                    <h3 style={{ marginBottom: '.75rem', fontWeight: 500 }}>{action.title}</h3>
+                    <p style={{ marginBottom: '0.5rem', fontWeight: 300 }}>{action.text}</p>
+                  </ShadowCard>
+                </Link>
+            ))}
+          </Row>
+        </DocsHeader>
+        <hr />
+        <Row>
+          <Link style={{ textDecoration: 'none' }} href={'https://discord.gg/ybKVQUWb4s'}>
+            <CenterCard>
+              <Discord style={{ width: '48px', height: '48px' }} />
+              <div>
+                <h3>Discord</h3>
+                <p>Join our Developer Community.</p>
+              </div>
+            </CenterCard>
           </Link>
-        </div>
-      </div>
-    </header>
+          <Link style={{ textDecoration: 'none' }} href={'https://gov.uniswap.org/'}>
+            <CenterCard>
+              <MessageCircle style={{ width: '48px', height: '48px' }} />
+              <div>
+                <h3>Forum</h3>
+                <p>Discuss governance and more.</p>
+              </div>
+            </CenterCard>
+          </Link>
+          <Link style={{ textDecoration: 'none' }} href={'https://github.com/0xSpaceShard'}>
+            <CenterCard>
+              <StyledIcon>
+                <GitHub style={{ width: '48px', height: '48px' }} />
+              </StyledIcon>
+              <div>
+                <h3>GitHub</h3>
+                <p>View all Nimbora repositories.</p>
+              </div>
+            </CenterCard>
+          </Link>
+        </Row>
+      </Container>
+    </Layout>
   );
 }
 
-export default function Home() {
+{/* export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
@@ -40,4 +266,4 @@ export default function Home() {
       </main>
     </Layout>
   );
-}
+} */}
