ebe0cb0386762e5a612092f59fb9424fdd5218a0
ioanSL <io@shardlabs.io>
2024-02-23 12:20:27 +0200

    Update Nimbora interaction text

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export const actions = [
     title: 'What is Nimbora',
     icon: HelpCircle,
     to: '/docs/concepts/overview',
-    text: 'Learn about Nimbora and how to interact with L1 & L2 strategies',
+    text: 'Learn how to interact with L1 & L2 strategies on Nimbora',
     image: Spacebbit
   },
   {
